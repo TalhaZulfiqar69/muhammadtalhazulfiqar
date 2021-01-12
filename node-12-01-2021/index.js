e8d60d7e479014bdac3e5231f9167c603ef0a15a
TalhaZulfiqar69 <talharana69@yahoo.com>
2021-01-12 16:03:56 +0500

    initial commit

--- node-12-01-2021/index.js
+++ node-12-01-2021/index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 app.listen(process.env.SERVER_PORT || 3000, (req, res) =>
-  console.log("Server is listened on the port", process.env.SERVER_PORT || 3000)
+  console.log("Server is listened on", process.env.SERVER_PORT || 3000)
 );
